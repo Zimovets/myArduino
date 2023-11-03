327b0e9d854188b78b4107a1a413ca2999b85fdc
Zimovets_Sergey <zimovets_s@ukr.net>
2023-11-03 18:28:43 +0200

    passive bazzer melody

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include <Arduino.h>
 
 int buzzerPin = 8;
-int buttonPin = 7;
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+
+  tone(buzzerPin, 1000, 2000);
 }
 
 void loop() {
-  int buttonState = digitalRead(buttonPin);
+  tone(buzzerPin, 440); // A4
+  delay(1000);
 
-  if (buttonState == LOW) {
-    digitalWrite(buzzerPin, HIGH);
-  }
+  tone(buzzerPin, 494); // B4
+  delay(1000);
 
-  if (buttonState == HIGH) {
-    digitalWrite(buzzerPin, LOW);
-  }
+  tone(buzzerPin, 523); // C4
+  delay(1000);
+
+  tone(buzzerPin, 587); // D4
+  delay(1000);
+
+  tone(buzzerPin, 659); // E4
+  delay(1000);
+
+  tone(buzzerPin, 698); // F4
+  delay(1000);
+
+  tone(buzzerPin, 784); // G4
+  delay(1000);
+
+  noTone(buzzerPin);
+  delay(1000);
 }
