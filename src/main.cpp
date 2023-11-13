d3208327683bdc7e7aca53bbcc2506dec7f8ba23
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-11-13 20:24:58 +0200

    test code for dc motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,19 @@
 #include <Arduino.h>
+#include <Adafruit_I2CDevice.h>
+#include <SPI.h>
+#include <AFMotor.h>
 
-#include <Stepper.h>
+AF_DCMotor motor(1);
 
-// Defines the number of steps per rotation
-const int stepsPerRevolution = 2038;
-
-// Creates an instance of stepper class
-// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
-
-void setup() {
-    // Nothing to do (Stepper Library sets pins as outputs)
+void setup() 
+{
+	//Set initial speed of the motor & stop
+	motor.setSpeed(255);
+	motor.run(FORWARD);
 }
 
-void loop() {
-	// Rotate CW slowly at 5 RPM
-	myStepper.setSpeed(5);
-	myStepper.step(stepsPerRevolution);
-	delay(1000);
+void loop() 
+{
 	
-	// Rotate CCW quickly at 10 RPM
-	myStepper.setSpeed(10);
-	myStepper.step(-stepsPerRevolution);
-	delay(1000);
 }
+
