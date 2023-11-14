a3afb0b9620c559134e8426744a04e714001c08c
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-11-14 22:22:39 +0200

    lcd v2 with standart librery

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,32 @@
-// LCD with i2c backpack test
-// This example shows how to transfer an in-memory buffer to the LCD at once.
-// A TwoWire instance is given as a parameter to lcd.begin to show how to pass a
-// non default wire interface. Urs Utzinger, 2020
-
 #include <Arduino.h>
-#include <Wire.h>
 
-#include "LiquidCrystal_PCF8574.h"
+#include <Wire.h> // библиотека для управления устройствами по I2C 
+#include <LiquidCrystal_I2C.h> // подключаем библиотеку для QAPASS 16x2
 
-LiquidCrystal_PCF8574 lcd(0x27);
+LiquidCrystal_I2C LCD(0x27,16,2); // присваиваем имя LCD для дисплея
 
 void setup() {
-    lcd.begin(16, 2);  // у нас экран 16 столбцов на 2 строки
-    lcd.setBacklight(255);  // установить яркость подсветки на максимум
-    lcd.clear();  // очистить экран и установить курсор в позицию 0, 0
+   LCD.init();    // инициализация LCD дисплея
+   LCD.backlight(); // включение подсветки дисплея
+
+   LCD.blink(); // включение мигающего курсора
+   delay(2000);
+   LCD.noBlink();
+   
+   LCD.setCursor(1, 0);    // ставим курсор на 1 символ первой строки
+   LCD.print("I LOVE");   // печатаем сообщение на первой строке
+   LCD.setCursor(8, 1);        // ставим курсор на 1 символ второй строки
+   LCD.print("ARDUINO");  // печатаем сообщение на второй строке
+   delay(2000);
+
+   LCD.cursor(); // включение текущей позиции курсора
+   delay(2000);
+   LCD.noCursor();
 }
+
 void loop() {
-	lcd.print("Hello World");
-	delay(3000);
-	lcd.setCursor(0, 1);
-	lcd.print(")))))");
-	delay(3000);
-	lcd.clear();
+   LCD.noBacklight(); // выключаем и включаем подсветку экрана
+   delay(1000);
+   LCD.backlight();
+   delay(1000);
 }
