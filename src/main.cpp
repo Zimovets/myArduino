194647876edc3a60876e945d8e260ae6143f7ca3
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-11-15 14:06:38 +0200

    temperature and humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,25 @@
 #include <Arduino.h>
 
-#include <Wire.h> // библиотека для управления устройствами по I2C 
-#include <LiquidCrystal_I2C.h> // подключаем библиотеку для QAPASS 16x2
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-LiquidCrystal_I2C LCD(0x27,16,2); // присваиваем имя LCD для дисплея
+DHT dht(2, DHT11);  // сообщаем на каком порту будет датчик
 
 void setup() {
-   LCD.init();    // инициализация LCD дисплея
-   LCD.backlight(); // включение подсветки дисплея
-
-   LCD.blink(); // включение мигающего курсора
-   delay(2000);
-   LCD.noBlink();
-   
-   LCD.setCursor(1, 0);    // ставим курсор на 1 символ первой строки
-   LCD.print("I LOVE");   // печатаем сообщение на первой строке
-   LCD.setCursor(8, 1);        // ставим курсор на 1 символ второй строки
-   LCD.print("ARDUINO");  // печатаем сообщение на второй строке
-   delay(2000);
-
-   LCD.cursor(); // включение текущей позиции курсора
-   delay(2000);
-   LCD.noCursor();
+   dht.begin();                // запускаем датчик DHT11
+   Serial.begin(9600);   // подключаем монитор порта
 }
 
 void loop() {
-   LCD.noBacklight(); // выключаем и включаем подсветку экрана
-   delay(1000);
-   LCD.backlight();
-   delay(1000);
+   // считываем температуру (t) и влажность (h)
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+
+   // выводим температуру (t) и влажность (h) на монитор порта
+   Serial.print("Humidity: ");
+   Serial.println(h);
+   Serial.print("Temperature: ");
+   Serial.println(t);
+   delay(5000);
 }
