4ff4dafcb1030626229bd5f41a40f302a4ed7213
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-11-17 15:59:05 +0200

    ultrasonic HC-SR04 without librery

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,31 @@
 #include <Arduino.h>
 
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-
-DHT dht(2, DHT11);  // сообщаем на каком порту будет датчик
+// Используйте свои значения для выводов
+int trigPin = 12;
+int echoPin = 11;
 
 void setup() {
-   dht.begin();                // запускаем датчик DHT11
-   Serial.begin(9600);   // подключаем монитор порта
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-   // считываем температуру (t) и влажность (h)
-   float h = dht.readHumidity();
-   float t = dht.readTemperature();
-
-   // выводим температуру (t) и влажность (h) на монитор порта
-   Serial.print("Humidity: ");
-   Serial.println(h);
-   Serial.print("Temperature: ");
-   Serial.println(t);
-   delay(5000);
+  int duration, distance;
+  // для большей точности установим значение LOW на пине Trig
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Теперь установим высокий уровень на пине Trig
+  digitalWrite(trigPin, HIGH);
+  // Подождем 10 микросекунд
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Узнаем длительность высокого сигнала на пине Echo
+  duration = pulseIn(echoPin, HIGH);
+  // Рассчитаем расстояние
+  distance = duration / 58;
+  // Выводим значение в Serial Monitor
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(100);
 }
