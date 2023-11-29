197de5fb27b997653ae00932d3901cf45164aeb4
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-11-29 16:44:02 +0200

    humidity sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,24 @@
 #include <Arduino.h>
 
-int ledPin = 13;  // инициализируем пин для светодиода
-int inputPin = 7;  // инициализируем пин для получения сигнала от пироэлектрического датчика движения
-int pirState = LOW;  // начинаем работу программы, предполагая, что движения нет
-int val = 0;  // переменная для чтения состояния пина
+int inputPin = A0;
+int zoomPin = 8;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // объявляем светодиод в качестве  OUTPUT
-  pinMode(inputPin, INPUT);  // объявляем датчик в качестве INPUT
+  pinMode(inputPin, INPUT);
+  pinMode(zoomPin, OUTPUT);
+  digitalWrite(zoomPin, LOW);
   Serial.begin(9600);
 }
 
 void loop() {
-  val = digitalRead(inputPin);  // считываем значение с датчика
-  if (val == HIGH) {  // проверяем, соответствует ли считанное значение HIGH
-    digitalWrite(ledPin, HIGH);  // включаем светодиод
-    if (pirState == LOW) {
-      // мы только что включили
-      Serial.println("Motion detected!");
-      // мы выводим на серийный монитор изменение, а не состояние
-      pirState = HIGH;
-    }
+  int value = analogRead(inputPin);
+  Serial.println(value);
+
+  if(value < 280) {
+    digitalWrite(zoomPin, HIGH);
   } else {
-    digitalWrite(ledPin, LOW); // выключаем светодиод
-    if (pirState == HIGH) {
-      // мы только что его выключили
-      Serial.println("Motion ended!");
-      // мы выводим на серийный монитор изменение, а не состояние
-      pirState = LOW;
-    }
+    digitalWrite(zoomPin, LOW);
   }
+
+  delay(1000);
 }
