bfb53d501e55a81a023fc819ee81b5ef5335737e
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-12-02 11:27:57 +0200

    tft display

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,38 @@
 #include <Arduino.h>
 
-int inputPin = A0;
-int zoomPin = 8;
-
-void setup() {
-  pinMode(inputPin, INPUT);
-  pinMode(zoomPin, OUTPUT);
-  digitalWrite(zoomPin, LOW);
-  Serial.begin(9600);
+#include <TFT.h>                 // Подключаем библиотеку TFT  
+#include <SPI.h>                 // Подключаем библиотеку SPI
+ 
+#define cs   10                  // Указываем пины cs
+#define dc   9                   // Указываем пины dc (A0)
+#define rst  8                   // Указываем пины reset
+ 
+TFT TFTscreen = TFT(cs, dc, rst);
+ 
+void setup() 
+{
+  TFTscreen.begin();
+  TFTscreen.background(0, 0, 0); // Очистим экран дисплея
+  TFTscreen.setTextSize(2);      // Устанавливаем размер шрифта
 }
-
-void loop() {
-  int value = analogRead(inputPin);
-  Serial.println(value);
-
-  if(value < 280) {
-    digitalWrite(zoomPin, HIGH);
-  } else {
-    digitalWrite(zoomPin, LOW);
-  }
-
-  delay(1000);
+ 
+void loop() 
+{
+/* 
+* Установка цвета фона TFTscreen.background ( r , g , b )
+* где, r, g и b являются значениями RGB для заданного цвета
+*/ 
+  TFTscreen.background ( 0 , 0 , 0 );
+/*
+* Команда установки цвета фона TFTscreen.stroke ( r , g , b )
+* где, r, g и b являются значениями RGB для заданного цвета
+*/  
+  TFTscreen.stroke(255, 0, 0);
+/*
+ * Вывод текста на дисплей TFTscreen.text("Hello, World!", x, y);
+ * где x и y координаты.
+*/
+  TFTscreen.text("Hello, World!", 6, 47);
+  TFTscreen.text("RobotChip", 30, 67);
+  delay(500);
 }
